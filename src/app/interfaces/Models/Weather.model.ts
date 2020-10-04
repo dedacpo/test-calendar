a769473c661682323bd68f031d1f3b5775fc8910
denisecalsavara <dedacpo@gmail.com>
2020-10-04 08:01:11 -0300

    Auto-generated commit

--- src/app/interfaces/Models/Weather.model.ts
+++
@@ -1,7 +0,0 @@
-export class Weather{
-    date: string
-    weatherId: number
-    weatherMain: string
-    weatherDescription: string
-    weatherIcon: string
-}
--- src/app/shared/store/store.actions.ts
+++
@@ -1,8 +0,0 @@
-import { createAction, props } from '@ngrx/store';
-import { Reminder } from 'src/app/interfaces/Models/Reminder';
-import { ISelectedYearAndMonth } from 'src/app/interfaces/SelectedYearAndMonth.model';
-
-
-export const selectedYearAndMonth = createAction('[Calendar Component] SelectedYearAndMonth', props<{selectedYearAndMonth}>());
-
-export const reminders = createAction('[Reminders Component] Reminders', props<{reminders: Reminder[]}>());
